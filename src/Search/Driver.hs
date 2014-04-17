440cf645728ef285269bc4853f80faf1a952e728
davnils <nilsson.dd+github+gravatarobscurity@gmail.com>
2014-04-17 12:03:59 +0200

    Print NFA, DFA, and matching inputs.

--- src/Search/Driver.hs
+++ src/Search/Driver.hs
@@ -3,12 +3,24 @@ module Main where
 import           Automata.RegExp
 import           Automata.DFA
 import           Automata.NFA
+import           Control.Monad (when)
 import qualified Data.Set as S
 import           Search.Lexer
 import           Search.Parser
 
 main :: IO ()
 main = do
-  [lang, regexp, input] <- fmap lines getContents
-  let compiled = parseRegExp . map fst $ alexScanTokens regexp
-  print . simulate input . fromNFAToDFA . exprToNFA (S.fromList lang) $ Fix compiled
+  (lang:regexp:input) <- fmap lines getContents
+  let cp = Fix . parseRegExp . map fst $ alexScanTokens (".*" ++ regexp)
+      nfa = exprToNFA (S.fromList lang) $ cp
+      dfa = fromNFAToDFA nfa
+
+  print cp
+  print nfa
+  print dfa
+
+  let check str = do
+        let res = simulate str dfa
+        when res $ putStrLn str
+
+  mapM_ check input
