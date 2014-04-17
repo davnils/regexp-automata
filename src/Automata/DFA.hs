4482a857efc994418068e1e13521a787f90864db
davnils <nilsson.dd+github+gravatarobscurity@gmail.com>
2014-04-17 22:10:01 +0200

    Partially working DFA minimization.

--- src/Automata/DFA.hs
+++ src/Automata/DFA.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE FlexibleInstances, TupleSections #-}
 module Automata.DFA
-(DFA, fromNFAToDFA, simulate)
+-- (DFA, fromNFAToDFA, minimize, simulate)
 where
 
 import qualified Automata.NFA as N
@@ -10,7 +10,7 @@ import qualified Control.Monad.Reader as R
 import qualified Control.Monad.State as ST
 import           Data.Foldable (forM_)
 import           Data.Functor.Foldable (cata)
-import           Data.List (intercalate, intersect)
+import           Data.List ((\\), intercalate, intersect, partition, union)
 import qualified Data.Map as M
 import           Data.Maybe (fromJust)
 import           Data.Semigroup ((<>), Semigroup)
@@ -31,6 +31,7 @@ data DFA k
 
 instance (Show a) => Show (DFA (S.Set a)) where
   show dfa = unlines $ [
+    show (_startState dfa),
     "digraph finite_state_machine {",
     "rankdir=LR;",
     "size=\"10,10\"",
@@ -88,8 +89,68 @@ closure = ST.execStateT $ do
       let new = S.union st layer
       lift (closure new) >>= ST.put
 
-minimize :: DFA (S.Set Int) -> DFA (S.Set Int)
-minimize = undefined
+type Node = S.Set Int
+
+-- find the unique (up to isomorphism) minimal representation
+-- minimize :: DFA Node -> DFA Node
+minimize dfa = (merge eq dfa, equivalent dfa)
+  where
+  -- only keep distinct pairs, construct [[Node]] indicating equivalence classes
+  eq = equivalent' dfa
+
+  -- construct a single node for each equivalence class
+  -- incoming edges to either node: add to replacement
+  -- outgoing edges from either node: add to replacement
+  -- self-references: add to replacement (i.e. looping on itself)
+  merge [] dfa         = dfa
+  merge (group:xs) dfa = merge xs (DFA (_alphabet dfa) (transferIn . transferOut $ _states dfa) newStart endStates 0)
+    where
+    newNode     = S.unions $ group <> [(S.singleton $ -1)]
+    newStart    = if (elem (_startState dfa) group) then newNode else (_startState dfa)
+    transferOut = M.map     $ \e -> if (elem e group) then newNode else e
+    transferIn  = M.mapKeys $ \(src, sym) -> if (elem src group) then (newNode, sym) else (src, sym)
+
+    -- delete all occurences of elements in group (set diff), and insert the new one (set union), if applicable
+    -- assumes that either (i) all entries are non-accept (2) all entries are accept
+    endStates   = if (removed == _endStates dfa) then (_endStates dfa) else (S.union (S.singleton newNode) removed)
+    removed     = S.difference (_endStates dfa) (S.fromList $ map S.singleton $ S.toList newNode)
+
+segment [] _         = []
+segment (x:xs) pairs = [x : (map snd emit)] <> segment xs pairs'
+  where
+  (emit, pairs') = partition (\(a,b) -> a == x || b == x) pairs
+
+equivalent' dfa = filter (\l -> length l > 1) . segment allStates . S.toList $ equivalent dfa
+  where
+  allStates = S.toList . S.fromList . map fst . M.keys $ _states dfa
+
+sortTuple = map (\(x,y) -> if x <= y then (x,y) else (y, x))
+
+-- locate inequivalent states (table fill method)
+-- equivalent = all possible pairs - distinguished
+equivalent :: DFA Node -> S.Set (Node, Node)
+equivalent dfa = S.fromList $ [(p, q) | p <- allStates, q <- allStates, p < q] \\ (go initial)
+  where
+  initial   = sortTuple $ [(p, q) | p <- endStates, q <- (allStates \\ endStates)]
+  endStates = S.toList $ _endStates dfa
+  allStates = S.toList . S.fromList . map fst . M.keys $ _states dfa
+
+  -- for every pair of disting.: for each a in alphabet: mark all source nodes as distinguished
+  -- repeat until no more nodes have been added
+  go :: [(Node, Node)] -> [(Node, Node)]
+  go disting
+    | length new == length disting = disting
+    | otherwise                   = go new
+    where
+    new  = union disting $ concatMap consider disting
+
+    -- for each alpha: check if there are incoming transitions, flag all source combinations
+    consider (p, q) = concatMap (considerSymbol p q) (S.toList $ _alphabet dfa)
+    considerSymbol p q s = removeDup $ [(a, b) | a <- concatMap (considerEntry p s) allStates, b <- concatMap (considerEntry q s) allStates, a /= b]
+    removeDup = S.toList . S.fromList . sortTuple
+    considerEntry dst sym src
+      | M.lookup (src, sym) (_states dfa) == Just dst = [src]
+      | otherwise = []
 
 simulate :: (Eq k, Ord k) => String -> DFA k -> Bool
 simulate str dfa = go (_startState dfa) str
--- src/Search/Driver.hs
+++ src/Search/Driver.hs
@@ -4,20 +4,62 @@ import           Automata.RegExp
 import           Automata.DFA
 import           Automata.NFA
 import           Control.Monad (when)
+import qualified Data.Map as M
 import qualified Data.Set as S
 import           Search.Lexer
 import           Search.Parser
 
+test :: IO ()
+test = do
+  let binds  = [((0,1), 0), ((0,0), 1), ((1,1), 0), ((1,0), 3), ((3,0), 4), ((3,1), 5), ((4,0), 4), ((4,1), 5), ((5,0), 5), ((5,1), 5)]
+      binds' = map (\((x, y), z) -> ((S.singleton x, head $ show y), S.singleton z)) binds
+      dfa    = DFA (S.fromList "01") (M.fromList binds') (S.singleton 0) (S.singleton $ S.singleton 5) 0
+      (minDFA, eq) = minimize dfa
+      sep    = putStrLn "-----------------------------------------------"
+
+  print dfa
+  sep
+  print minDFA
+  sep
+  print $ eq -- map (\(x, y) -> (head $ S.toList x, head $ S.toList y)) eq
+  sep
+
+test2 :: IO ()
+test2 = do
+  let binds  = [((0,0), 1), ((0,1), 5), ((1,0), 6), ((1,1), 2), ((2,0), 0), ((2,1), 2),
+                ((3,0), 2), ((3,1), 6), ((4,0), 7), ((4,1), 5), ((5,0), 2), ((5,1), 6),
+                ((6,0), 6), ((6,1), 4), ((7,0), 6), ((7,1), 2)]
+      binds' = map (\((x, y), z) -> ((S.singleton x, head $ show y), S.singleton z)) binds
+      dfa    = DFA (S.fromList "01") (M.fromList binds') (S.singleton 0) (S.singleton $ S.singleton 2) 0
+      (minDFA, eq) = minimize dfa
+      sep    = putStrLn "-----------------------------------------------"
+
+  print dfa
+  sep
+  print minDFA
+  sep
+  print $ map (\(x,y) -> (head $ S.toList x, head $ S.toList y)) (S.toList eq) -- map (map $ head . S.toList) eq
+  sep
+
 main :: IO ()
 main = do
   (lang:regexp:input) <- fmap lines getContents
-  let cp = Fix . parseRegExp . map fst $ alexScanTokens (".*" ++ regexp)
-      nfa = exprToNFA (S.fromList lang) $ cp
-      dfa = fromNFAToDFA nfa
+  let cp     = Fix . parseRegExp . map fst $ alexScanTokens (".*" ++ regexp)
+      nfa    = exprToNFA (S.fromList lang) $ cp
+      dfa    = fromNFAToDFA nfa
+      -- (minDFA, eq) = minimize dfa
+      sep    = putStrLn "-----------------------------------------------"
 
   print cp
+  sep
   print nfa
+  sep
   print dfa
+  sep
+  -- print minDFA
+  sep
+  -- print eq
+  sep
 
   let check str = do
         let res = simulate str dfa
